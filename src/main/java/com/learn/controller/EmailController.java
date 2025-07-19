fe45c9eaeb60fcbf805cc48c38a7fb50b5b5a5d2
NguyenNguDang <dq0202345@gmail.com>
2025-07-20 03:47:04 +0700

    Using SendGrid send Email

---
+++ src/main/java/com/learn/controller/EmailController.java
@@ -0,0 +1,23 @@
+package com.learn.controller;
+
+import com.learn.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j(topic = "EMAIL-CONTROLLER")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @GetMapping("/send-email")
+    public void sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
+        log.info("Sending email to {}", to);
+        emailService.sendEmail(to, subject, body);
+    }
+}
